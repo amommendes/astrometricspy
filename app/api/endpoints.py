6b86560af46009908cd6140e29a5ca1435230c0f
amommendes <amommendes@hotmail.com>
2020-04-04 20:50:01 -0300

    Gauge inside api

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,11 +1,12 @@
 from fuzzywuzzy import fuzz
 from PyDictionary import PyDictionary
 from flask_restful import Resource
-from app import FLASK_REQUEST_COUNT
+from app import FLASK_REQUEST_COUNT, FLASK_REQUEST_GAUGE
 
 class WordMeaning(Resource):
     def get(self, word):
         FLASK_REQUEST_COUNT.labels('GET', 'WordMeaning', 200).inc()
+        FLASK_REQUEST_GAUGE.labels("GET", "WordMeaning").inc()
         dictionary = PyDictionary()
         return dictionary.meaning(word), 200
 
