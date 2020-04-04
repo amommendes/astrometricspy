1d1c21b959cbf9a12ebd0bd4630cc9d2c2384d6c
amommendes <amomluiz@SPF1LT-PJ000000.ldap.quintoandar.com.br>
2020-04-04 20:43:37 -0300

    Dockerizing environment with Flask + Prometheus + Grafana

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 import os
 from flask import Flask, request, g
 from flask_restful import Api
-from prometheus_client import start_http_server, Counter, Histogram
+from prometheus_client import start_http_server, Counter, Histogram, Gauge
 import time
 
-
-FLASK_REQUEST_COUNT = Counter('flask_request_count', 'Flask Request Count', ['method', 'endpoint', 'http_status'])
+FLASK_REQUEST_COUNT = Counter('flask_request_counter', 'Flask Request Count', ['method', 'endpoint', 'http_status'])
 FLASK_REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Flask Request Latency', ['method', 'endpoint'])
-
+FLASK_REQUEST_GAUGE = Gauge('flask_gauge_metric', 'Flask Gauge', ['method', 'endpoint'])
 
 def before_request_func():
     g.request_start_time = time.time()
 
 def after_request(response):
+        print("Before request!")
         request_latency = time.time() - g.request_start_time
         FLASK_REQUEST_LATENCY.labels(request.method, request.path).observe(request_latency)
         FLASK_REQUEST_COUNT.labels(request.method, request.path, response.status_code).inc()
+        FLASK_REQUEST_GAUGE.labels(request.method, request.path).inc()
         return response
 
 def create_app(test_config=None):
@@ -27,7 +28,13 @@ def create_app(test_config=None):
     api = Api(app)
     app.before_request(before_request_func)
     app.after_request(after_request)
-    start_http_server(9090)
+    try:
+        print( "I'm using 9001" )
+        start_http_server(9001)
+    except OSError as error:
+        print("It seems that port is already in use {}".format(error))
+        start_http_server(9001+1)
+
 
     if test_config is None:
         # load the instance instance, if it exists, when not testing
@@ -39,5 +46,9 @@ def create_app(test_config=None):
     from app.api import endpoints
     api.add_resource(endpoints.WordMeaning, "/words/meaning/<word>",)
     api.add_resource(endpoints.MatchWords, "/words/match/<word1>/<word2>",)
+
+    @app.route('/stats', methods=['GET'])
+    def metrics():
+        return generate_latest(registry), 200
 
     return app
---
+++ app/start_app.sh
@@ -0,0 +1,4 @@
+
+#Start Flask
+echo $(date "+%Y-%m-%d %H:%M:%S") [INFO] Starting Flask
+flask run --host='0.0.0.0' --port=5000
